5a340d1b9f132fe9c7eef27fdff5589768a91e5b
Bruno Gustavo Rocha <bruno.lusa@yahoo.com.br>
2024-10-18 21:37:49 -0300

    Mudar nome de arquivos e referenciar corretamente

--- Alertapp/src/router/index.ts
+++ Alertapp/src/router/index.ts
@@ -17,15 +17,19 @@ const routes: Array<RouteRecordRaw> = [
       },
       {
         path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
+        component: () => import('@/views/PaginaInicial.vue')
       },
       {
         path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
+        component: () => import('@/views/PaginaNoticias.vue')
       },
       {
         path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
+        component: () => import('@/views/PaginaAlerta.vue')
+      },
+      {
+        path: 'tab4',
+        component: () => import('@/views/PaginaMapa.vue')
       }
     ]
   }
--- Alertapp/src/views/PaginaNoticias.vue
+++ Alertapp/src/views/PaginaNoticias.vue
@@ -2,22 +2,20 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 3</ion-title>
+        <ion-title>Página de Notícias</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 3</ion-title>
+          <ion-title size="large">Página de Notícias</ion-title>
         </ion-toolbar>
       </ion-header>
-
-      <ExploreContainer name="Tab 3 page" />
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
 </script>
